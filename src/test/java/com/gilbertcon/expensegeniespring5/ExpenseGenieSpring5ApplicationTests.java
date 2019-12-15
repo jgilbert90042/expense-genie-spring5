1749a9b553d32638d55b373b54b1b73d49a4efe2
Joe Gilbert <joe@gilbertcon.com>
2019-12-14 20:39:04 -0500

    Added in base integration test

---
+++ src/test/java/com/gilbertcon/expensegeniespring5/ExpenseGenieSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilbertcon.expensegeniespring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseGenieSpring5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
